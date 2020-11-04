923d147510dead7fa7f2651678829aa1a85eb90e
robsonsilv4 <robsonsilv410@gmail.com>
2020-11-04 12:50:29 -0300

    feat: add numbers state notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_pro/number_state_notifier.dart';
 
 void main() {
   runApp(
@@ -31,11 +32,17 @@ final numberStateProvider = StateProvider<int>((ref) {
   return 11;
 });
 
+final numbersStateNotifierProvider =
+    StateNotifierProvider<NumbersNotifier>((ref) {
+  return NumbersNotifier();
+});
+
 class MyHomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final number = watch(numberProvider);
     final numberState = watch(numberStateProvider).state;
+    final numbersStateNotifier = watch(numbersStateNotifierProvider.state);
 
     void increment(BuildContext context) {
       context.read(numberStateProvider).state++;
@@ -43,11 +50,16 @@ class MyHomePage extends ConsumerWidget {
 
     return Scaffold(
       body: Center(
-        child: Text(numberState.toString()),
+        child: ListView.builder(
+          itemCount: numbersStateNotifier.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Text(numbersStateNotifier[index].toString());
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => increment(context),
+        onPressed: () => context.read(numbersStateNotifierProvider).add(1),
       ),
     );
   }

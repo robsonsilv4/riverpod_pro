e0261c488a4cf78eaa32f71052661eb112b8e77a
robsonsilv4 <robsonsilv410@gmail.com>
2020-11-04 10:58:28 -0300

    initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# riverpod_pro
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/tech/robsonsilva/riverpod_pro/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.robsonsilva.riverpod_pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
